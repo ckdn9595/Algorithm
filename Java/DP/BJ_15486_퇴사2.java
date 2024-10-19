dae00233f915915c12ffe9de4642681cb085b6bd
JiwooLee <ckdn9595@gmail.com>
2024-10-19 15:09:37 +0900

    BJ_15486_퇴사2

--- Java/DP/BJ_15486_퇴사2.java
+++ Java/DP/BJ_15486_퇴사2.java
@@ -5,12 +5,14 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BJ_15486_퇴사2 {
+    
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int [] dp = new int [N+1];
         int [] t = new int [N];
         int [] p = new int [N];
+
 
         for (int i = 0; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
